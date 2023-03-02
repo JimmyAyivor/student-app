7879564e357bdec5b8e2dbf59021c447b478ca41
Jimmy Ayivor <jimmyayivor@gmail.com>
2023-03-02 09:52:46 -0500

    add support for local and remote

--- student-app-backend/db/index.js
+++ student-app-backend/db/index.js
@@ -1,15 +1,27 @@
-const pgp = require('pg-promise')();
+// http://vitaly-t.github.io/pg-promise/module-pg-promise.html
+const pgp = require("pg-promise")();
+require("dotenv").config();
 
-require('dotenv').config();
-
-const databaseUrl = process.env.DB_URL;
-
-const cn = {
-  connectionString: databaseUrl,
-  allowExitOnIdle: true,
-  max: 30,
-};
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
+  process.env;
+// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+      password: PG_PASSWORD,
+    };
 
 const db = pgp(cn);
 
 module.exports = db;
+
