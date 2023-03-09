8a2cc48136a05fdf5b86f475a9321f815aac5e47
Jimmy Ayivor <12377575+JimmyAyivor@users.noreply.github.com>
2023-03-08 19:31:35 -0500

    Update StudentList.js

--- student-app-frontend/src/components/StudentList/StudentList.js
+++ student-app-frontend/src/components/StudentList/StudentList.js
@@ -1,11 +1,92 @@
-import StudentCard from "../StudentCard/StudentCard";
-import "./StudentList.css"
+import { useState } from 'react';
+import StudentCard from '../StudentCard/StudentCard';
+
+import './StudentList.css';
+
 const StudentList = ({ studentData }) => {
+  const [searchInput, setSearchInput] = useState('');
+  const [expanded, setExpanded] = useState([]);
+
+  const handleToggleExpanded = (id) => {
+    
+    if (!expanded.includes(id)) {
+   
+      const newExpanded = [...expanded, id];
+      setExpanded(newExpanded);
+    } else {
+      
+      const removed = expanded.filter((currId) => currId !== id);
+      setExpanded(removed);
+    }
+  };
+
+  const handleExpandAll = () => {
+
+    const allIds = studentData.map((student) => student.id);
+    setExpanded(allIds);
+  };
+
+  const handleCollapseAll = () => {
+    setExpanded([]);
+  };
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  let dataToDisplay = studentData;
+
+  
+  if (searchInput) {
+   
+    dataToDisplay = studentData.filter((student) => {
+     
+      const { firstName, lastName } = student;
+      const fullName = `${firstName} ${lastName}`.toLowerCase();
+
+      return fullName.includes(searchInput.toLowerCase());
+    });
+  }
+
+  const renderContent = () => {
+    let contentClassName = 'StudentList__content';
+
+    if (dataToDisplay.length === 0) {
+      contentClassName += ' StudentList__content--center';
+      return (
+        <div className={contentClassName}>No results for {searchInput}</div>
+      );
+    } else {
+      return (
+        <div className={contentClassName}>
+    
+          {dataToDisplay.map((student) => (
+            <StudentCard
+              key={student.id}
+              student={student}
+              expanded={expanded.includes(student.id)}
+              onClick={() => handleToggleExpanded(student.id)}
+            />
+          ))}
+        </div>
+      );
+    }
+  };
+
+  console.log(`<StudentList /> rendered! searchInput = ${searchInput}`);
   return (
     <div className="StudentList">
-      {studentData && studentData.map((student) => (
-        <StudentCard key={student.id} student={student} />
-      ))}
+      <div className="StudentList__controls">
+        <input
+          value={searchInput}
+          type="text"
+          placeholder="Search by name"
+          onChange={handleChange}
+        />
+        <button onClick={handleExpandAll}>Expand All</button>
+        <button onClick={handleCollapseAll}>Collapse All</button>
+      </div>
+      {renderContent()}
     </div>
   );
 };
